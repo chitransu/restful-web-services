903127633d74cd0383328686a01f26ead40cba8d
chitransu007 <chitransu007@gmail.com>
2019-12-07 08:07:44 +0530

    added rest application

---
+++ src/main/java/com/i28minutes/rest/webservices/restfulwebservices/RestfulWebServicesApplication.java
@@ -0,0 +1,13 @@
+package com.i28minutes.rest.webservices.restfulwebservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestfulWebServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestfulWebServicesApplication.class, args);
+	}
+
+}
